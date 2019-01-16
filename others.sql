f986e8f3aee3ed753addeec120bc77808963624c
analytics-piyo <32300937+AnalyticsPiyo@users.noreply.github.com>
2019-01-17 01:23:28 +0900

    Update others.sql

--- others.sql
+++ others.sql
@@ -3,3 +3,5 @@
     (CHAR_LENGTH("列名") - CHAR_LENGTH(REPLACE("列名", 'カウントしたい文字列', ''))) / CHAR_LENGTH('カウントしたい文字列') AS "cnt"
   FROM
     tmp0
+/* psqlからのcopy文 */
+\copy "table名" from 'file path'  with encoding 'UTF8' csv delimiter ',' header ;
